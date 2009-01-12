8da5545f26438251838ac11f29675a20bd85e63b
David Glasser <glasser@davidglasser.net>
2009-01-12 14:19:42 -0500

    Initial version of somelinks.

--- main.py
+++ main.py
@@ -16,23 +16,67 @@
 #
 
 
-
+import os
 
 import wsgiref.handlers
 
-
+from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import util
 
 
-class MainHandler(webapp.RequestHandler):
+class Link(db.Model):
+  url = db.StringProperty(required=True)
+  description = db.TextProperty(required=True)
 
+
+class ListHandler(webapp.RequestHandler):
   def get(self):
-    self.response.out.write('Hello world!')
+    links = Link.all()
+    path = os.path.join(os.path.dirname(__file__), 'list.html')
+    self.response.out.write(template.render(path, {
+        'links': Link.all(),
+        'admin': users.is_current_user_admin(),
+        }))
+
+
+class LoginHandler(webapp.RequestHandler):
+  @util.login_required
+  def get(self):
+    self.redirect(ListHandler.get_url())
+
+
+class AddHandler(webapp.RequestHandler):
+  def post(self):
+    if not users.is_current_user_admin():
+      self.redirect(ListHandler.get_url())
+      return
+    link = Link(url=self.request.get('url'),
+                description=self.request.get('description'))
+    link.put()
+    self.redirect(ListHandler.get_url())
+
+
+class DeleteHandler(webapp.RequestHandler):
+  def get(self, link_id):
+    if not users.is_current_user_admin():
+      self.redirect(ListHandler.get_url())
+      return
+    link_id = long(link_id)
+    link = Link.get_by_id(link_id)
+    link.delete()
+    self.redirect(ListHandler.get_url())
 
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler)],
-                                       debug=True)
+  application = webapp.WSGIApplication([
+      ('/', ListHandler),
+      ('/login/?', LoginHandler),
+      ('/add', AddHandler),
+      ('/delete/(\\d+)', DeleteHandler),
+      ])
   wsgiref.handlers.CGIHandler().run(application)
 
 
